a5fc5efa3a36cfb43d0d11bf93c1d894d6629060
piiiiii41 <87687195+piiiiii41@users.noreply.github.com>
2021-07-27 17:25:18 +0900

    up2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  alert();
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  console.log(li);
+
+  // button（完了）タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+
+  // button（削除）タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  console.log(deleteButton);
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
